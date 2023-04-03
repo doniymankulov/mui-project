31ae35526f956882d1c9ef3544dc5e035cc32523
doniymankulov <120245637+doniymankulov@users.noreply.github.com>
2023-04-03 19:00:42 +0600

    btn (#1)

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,27 +5,44 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import {Card, CardMedia} from "@mui/material";
 import Box from "@mui/material/Box";
+
 const List = ({readProduct, product}) => {
 
-    useEffect(()=>{
+    useEffect(() => {
         readProduct()
     }, [])
 
     return (
-        <div style={{display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', marginTop: '20px'}} className="list--card">
+        <div style={{display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', marginTop: '20px'}}
+             className="list--card">
 
             {
-                product.map((item, index)=>{
+                product.map((item, index) => {
                     return (
-                        <Box sx={{ border: '2px solid #181818', textAlign: 'center', marginTop: '10px', borderRadius: '6px' }}>
-                                <img src={item.image} width="300px" alt=""/>
-                                <div >
-                                    <h1 style={{marginTop:'-10px'}}>{item.title}</h1>
-                                 <div style={{marginTop: '-30px', display: 'flex', justifyContent: 'space-around'}}>
-                                     <p>{item.descr}</p>
-                                     <p>{item.price}</p>
-                                 </div>
+                        <Box sx={{
+                            border: '2px solid #181818',
+                            textAlign: 'center',
+                            marginTop: '10px',
+                            borderRadius: '6px'
+                        }}>
+                            <img src={item.image} width="300px" alt=""/>
+                            <div>
+                                <h1 style={{marginTop: '-10px'}}>{item.title}</h1>
+                                <div style={{marginTop: '-30px', display: 'flex', justifyContent: 'space-around'}}>
+                                    <p>{item.descr}</p>
+                                    <p>{item.price}</p>
+                                    <button style={{
+                                        padding: "5px 20px",
+                                        background: "red",
+                                        color: "white",
+                                        border: "none",
+                                        outline: "none",
+                                        borderRadius: "10px",
+
+                                    }}>Delete
+                                    </button>
                                 </div>
+                            </div>
 
                         </Box>
 
